436d5b27a5c4a193e84d9fdbb85faadfbc6d957c
zygof <46953622+zygof@users.noreply.github.com>
2022-04-29 16:31:08 +0200

    Test n°1 - (green) Une fois que la date de péremption est passée, la qualité se dégrade deux fois plus rapidement.

--- test/gilded-rose.spec.ts
+++ test/gilded-rose.spec.ts
@@ -1,12 +1,10 @@
-import { expect } from 'chai';
-import { Item, GildedRose } from '../app/gilded-rose';
+import { expect } from "chai";
+import { Item, GildedRose } from "../app/gilded-rose";
 
-describe('Gilded Rose', function () {
-
-    it('should foo', function() {
-        const gildedRose = new GildedRose([ new Item('foo', 0, 0) ]);
-        const items = gildedRose.updateQuality();
-        expect(items[0].name).to.equal('fixme');
-    });
-
+describe("Gilded Rose", function () {
+  it("Une fois que la date de péremption est passée, la qualité se dégrade deux fois plus rapidement.", () => {
+    const gildedRose = new GildedRose([new Item("+5 Dexterity Vest", 0, 2)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).to.equal(0);
+  });
 });
